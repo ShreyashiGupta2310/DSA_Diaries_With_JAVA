aafe94463d47a3272161e76039547cfabe40b79c
Shreyashi <shreyashigupta2310@gmail.com>
2026-02-14 23:58:41 +0530

    added search function in a 2d array but it's not working , but fix it soon

--- Matrices/Two_D_array_creation.java
+++ Matrices/Two_D_array_creation.java
@@ -3,6 +3,30 @@ package Matrices;
 import java.util.*;
 
 public class Two_D_array_creation {
+
+//search an element
+public static boolean search(int[][] array,int element){
+    //for row length
+        int m=array.length;
+        //for column length
+        int n=array[0].length;
+  
+  
+    for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+              if(array[i][j]==element){
+                System.out.println("found at index"+"("+i+","+j+")");
+                return true;
+              }
+            }
+             System.out.println();
+        }
+        System.out.print("key nahi mili");
+        return false;
+}
+
+
+
     public static void main(String args[]){
         Scanner obj=new Scanner(System.in);
         int matrix[][]=new int[3][3];
@@ -10,7 +34,7 @@ public class Two_D_array_creation {
         int m=matrix.length;
         //for column length
         int n=matrix[0].length;
-//input the array
+    //input the array
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                matrix[i][j]= obj.nextInt();
@@ -25,5 +49,9 @@ public class Two_D_array_creation {
             }
              System.out.println();
         }
+
+      boolean p= search(matrix,6);
+      
+
     }
 }
