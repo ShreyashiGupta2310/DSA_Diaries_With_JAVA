039018821a1176f8791cb0325386b14f979a2d48
Shreyashi <shreyashigupta2310@gmail.com>
2026-02-18 21:20:40 +0530

    Reduced the time complexity from O(n^2) --> O(n)

--- Matrices/Sum_of_Diagonal.java
+++ Matrices/Sum_of_Diagonal.java
@@ -4,20 +4,32 @@ import java.util.*;
 public class Sum_of_Diagonal {
     public static int DiagonalSum(int matrix[][]){
         int sum=0;
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
-                //for primary diagonal
-                if(i==j){
-             sum=sum+matrix[i][j];
-                }
-                //for secondary diagonal
-                else if(i+j==matrix.length-1){
-                    sum=sum+matrix[i][j];
-                }
+        // for(int i=0;i<matrix.length;i++){
+        //     for(int j=0;j<matrix[0].length;j++){
+        //         //for primary diagonal
+        //         if(i==j){
+        //      sum=sum+matrix[i][j];
+        //         }
+        //         //for secondary diagonal
+        //         else if(i+j==matrix.length-1){
+        //             sum=sum+matrix[i][j];
+        //         }
 
+        //     }
+        // }
+
+        for(int i=0; i<matrix.length ;i++){
+            //primary diagonal condition
+            sum=sum+matrix[i][i];
+            //secondary diagonal condition
+            //logic -----> if i+j=matrix.length-1 then it is secondary diagonal so in order to replace j (i.e the second loop )  j=natix.length-i-1 (just a simple math)
+
+            if(i !=matrix.length-i-1)// i.e secondary diagonal condition not full filled also this condition will help in odd number of elements like 5 by 5 matrix 
+            {
+                sum+=matrix[i][matrix.length-i-1];
             }
-        }
 
+        }
         return sum;
     }
     public static void main(String args[]){
