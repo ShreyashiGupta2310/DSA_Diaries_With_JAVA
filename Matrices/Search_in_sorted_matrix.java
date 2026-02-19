c7dc8142fc6258ea5f7382e58b6df1c4c3b7e1c7
Shreyashi <shreyashigupta2310@gmail.com>
2026-02-19 23:44:04 +0530

    adding a seach function(which is not giving me desired output) in a sorted 2D matrix

---
+++ Matrices/Search_in_sorted_matrix.java
@@ -0,0 +1,27 @@
+package Matrices;
+
+public class Search_in_sorted_matrix {
+    public static boolean Search(int matrix[][],int key){
+        int row=0;
+        int col=matrix[0].length-1;
+        while(row>=0 && col>=matrix[0].length-1){
+            if(matrix[row][col]==key){
+                System.out.print("key"+key+" found at "+"("+row +","+col +")");
+                return true;
+            }
+            else if(key>matrix[row][col])
+                row++;
+            else if(key<matrix[row][col])
+                col--;
+        }
+        System.out.println("key not found");
+        return false;
+    }
+    public static void main(String args[]){
+        int matrix[][]={{1,2,3,4},
+                        {5,6,7,8}, 
+                        {9,10,11,12}, 
+                        {13,14,15,16}};
+        Search(matrix,6);
+    }
+}
