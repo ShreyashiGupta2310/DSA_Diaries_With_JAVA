32d347979b84fa54b10d4592dd1b56408d819c7f
Shreyashi <shreyashigupta2310@gmail.com>
2026-03-06 12:06:42 +0530

    Add fully optimized recursive power function (O(log n))

--- Recursion/OptimizedPower.java
+++ Recursion/OptimizedPower.java
@@ -1,11 +1,17 @@
 package Recursion;
 
 public class OptimizedPower {
+
+
+// This implementation calculates power recursively but calls
+// the function twice for half powers, resulting in O(n) time complexity.
+// Will optimize later for O(log n).
     public static int Optimizedpower(int number, int power){
         if(power==0)
             return 1;
 
-        int halfpower=Optimizedpower(number,power/2)*Optimizedpower(number,power/2);
+        int halfpower=Optimizedpower(number,power/2)* Optimizedpower(number,power/2);
+      
 
         if(power%2!=0)
             return number *halfpower;
@@ -13,11 +19,26 @@ public class OptimizedPower {
         return halfpower;
     }
 
+// Refactored OptimizedPower to compute half powers once,
+// reducing redundant recursion and achieving true O(log n) time complexity.
+
+
+        public static int Optimizedpowerplus(int number, int power){
+        if(power==0)
+            return 1;
+
+        int halfpower=Optimizedpowerplus(number,power/2);
+        int powerplus =halfpower*halfpower;
+
+        if(power%2!=0)
+            return number *powerplus;
+
+        return powerplus;
+    }
+
+
 
     public static void main(String args[]){
-        System.out.println(Optimizedpower(2,10));
+        System.out.println(Optimizedpowerplus(2,10));
     }
 }
-// This implementation calculates power recursively but calls
-// the function twice for half powers, resulting in O(n) time complexity.
-// Will optimize later for O(log n).
